68f0053aab309f2590bb3c195349d869c3c1b67b
Luca Orion <lucaorion@Mac.ht.home>
2025-06-12 17:13:34 -0400

    Update base path for phonktopia repository

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: 'src',
   publicDir: 'assets',
-  base: '/Phonkweb-2/',
+  base: '/phonktopia/',
   server: {
     port: 3000,
     open: true

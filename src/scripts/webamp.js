36f20fee4dc48d9f26b644f5014e3b8d00331c02
Luca Orion <lucaorion@Lucas-MacBook-Pro.local>
2025-06-13 16:51:35 -0400

    Update links in webamp.js

--- src/scripts/webamp.js
+++ src/scripts/webamp.js
@@ -9,145 +9,145 @@ document.addEventListener('DOMContentLoaded', function () {
       initialTracks: [
         {
           metaData: { artist: "Phonk", title: "Funk Do Inferno" },
-          url: "https://pub-47d44b4e2e4c43d7922bf3cc7715f8b0.r2.dev/funk-do-inferno-slowed.mp3",
+          url: "https://audio.phonktopia.com/funk-do-inferno-slowed.mp3",
         },
         // ... (all your other tracsk
         {
           metaData: { artist: "Phonk", title: "Pita O Funk" },
-          url: "https://pub-47d44b4e2e4c43d7922bf3cc7715f8b0.r2.dev/pita-o-funk-super-slowed.mp3",
+          url: "https://audio.phonktopia.com/pita-o-funk-super-slowed.mp3",
         },
         {
           metaData: {
             artist: "Phonk",
             title: "Midnight Drift",
           },
-          url: "https://pub-47d44b4e2e4c43d7922bf3cc7715f8b0.r2.dev/midnight-drift.mp3",
+          url: "https://audio.phonktopia.com/midnight-drift.mp3",
         },
         {
           metaData: {
             artist: "Phonk",
             title: "Allo Funk",
           },
-          url: "https://pub-47d44b4e2e4c43d7922bf3cc7715f8b0.r2.dev/allo-funk-sayfalse-rd12.mp3",
+          url: "https://audio.phonktopia.com/allo-funk-sayfalse-rd12.mp3",
         },
         {
           metaData: {
             artist: "Phonk",
             title: "Beat Ansiosa",
           },
-          url: "https://pub-47d44b4e2e4c43d7922bf3cc7715f8b0.r2.dev/beat-ansiosa-hardstyle-remix-slowed.mp3",
+          url: "https://audio.phonktopia.com/beat-ansiosa-hardstyle-remix-slowed.mp3",
         },
         {
           metaData: {
             artist: "Phonk",
             title: "Brazilian Funk X Slide",
           },
-          url: "https://pub-47d44b4e2e4c43d7922bf3cc7715f8b0.r2.dev/brazilian-funk-x-slide-funk-x-phonk-type-beat-pega-2-prod-pandemxnium.mp3",
+          url: "https://audio.phonktopia.com/brazilian-funk-x-slide-funk-x-phonk-type-beat-pega-2-prod-pandemxnium.mp3",
         },
         {
           metaData: {
             artist: "Phonk",
             title: "Dimensão da Antimatéria",
           },
-          url: "https://pub-47d44b4e2e4c43d7922bf3cc7715f8b0.r2.dev/dimensa%CC%83o-da-antimate%CC%81ria-slowed-eiby.mp3",
+          url: "https://audio.phonktopia.com/dimensa%CC%83o-da-antimate%CC%81ria-slowed-eiby.mp3",
         },
         {
           metaData: {
             artist: "Phonk",
             title: "Egoística",
           },
-          url: "https://pub-47d44b4e2e4c43d7922bf3cc7715f8b0.r2.dev/egoI%CC%81stica-slowed-to-perfection.mp3",
+          url: "https://audio.phonktopia.com/egoI%CC%81stica-slowed-to-perfection.mp3",
         },
         {
           metaData: {
             artist: "Phonk",
             title: "Funk Do Inferno",
           },
-          url: "https://pub-47d44b4e2e4c43d7922bf3cc7715f8b0.r2.dev/funk-do-inferno-slowed.mp3",
+          url: "https://audio.phonktopia.com/funk-do-inferno-slowed.mp3",
         },
         {
           metaData: {
             artist: "Phonk",
             title: "Gonza Funk",
           },
-          url: "https://pub-47d44b4e2e4c43d7922bf3cc7715f8b0.r2.dev/gonza-funk.mp3",
+          url: "https://audio.phonktopia.com/gonza-funk.mp3",
         },
         {
           metaData: {
             artist: "Phonk",
             title: "HIIT 3 2KE",
           },
-          url: "https://pub-47d44b4e2e4c43d7922bf3cc7715f8b0.r2.dev/hiit-3-2ke-clean-version.mp3",
+          url: "https://audio.phonktopia.com/hiit-3-2ke-clean-version.mp3",
         },
         {
           metaData: {
             artist: "Phonk",
             title: "Montagem Explosao Extrema",
           },
-          url: "https://pub-47d44b4e2e4c43d7922bf3cc7715f8b0.r2.dev/montagem-explosao-extrema.mp3",
+          url: "https://audio.phonktopia.com/montagem-explosao-extrema.mp3",
         },
         {
           metaData: {
             artist: "Phonk",
             title: "Montagem Forçe Bater",
           },
-          url: "https://pub-47d44b4e2e4c43d7922bf3cc7715f8b0.r2.dev/montagem-forC%CC%A7e-bater.mp3",
+          url: "https://audio.phonktopia.com/montagem-forC%CC%A7e-bater.mp3",
         },
         {
           metaData: {
             artist: "Phonk",
             title: "Montagem Mare Grana",
           },
-          url: "https://pub-47d44b4e2e4c43d7922bf3cc7715f8b0.r2.dev/montagem-mare-grana-hardstyle-remix.mp3",
+          url: "https://audio.phonktopia.com/montagem-mare-grana-hardstyle-remix.mp3",
         },
         {
           metaData: {
             artist: "Phonk",
             title: "Montagem Mare Grana Nakama",
           },
-          url: "https://pub-47d44b4e2e4c43d7922bf3cc7715f8b0.r2.dev/montagem-mare-grana-nakama-imaro-mc-gil-do-andarai-nakama.mp3",
+          url: "https://audio.phonktopia.com/montagem-mare-grana-nakama-imaro-mc-gil-do-andarai-nakama.mp3",
         },
         {
           metaData: {
             artist: "Phonk",
             title: "MTG Zona Fatal",
           },
-          url: "https://pub-47d44b4e2e4c43d7922bf3cc7715f8b0.r2.dev/mtg-zona-fatal.mp3",
+          url: "https://audio.phonktopia.com/mtg-zona-fatal.mp3",
         },
         {
           metaData: {
             artist: "Phonk",
             title: "Mxng0 Slide",
           },
-          url: "https://pub-47d44b4e2e4c43d7922bf3cc7715f8b0.r2.dev/mxng0-slide-de-ritmo.mp3",
+          url: "https://audio.phonktopia.com/mxng0-slide-de-ritmo.mp3",
         },
         {
           metaData: {
             artist: "Phonk",
             title: "Nitox",
           },
-          url: "https://pub-47d44b4e2e4c43d7922bf3cc7715f8b0.r2.dev/nitox.mp3",
+          url: "https://audio.phonktopia.com/nitox.mp3",
         },
         {
           metaData: {
             artist: "Phonk",
             title: "Pita O Funk",
           },
-          url: "https://pub-47d44b4e2e4c43d7922bf3cc7715f8b0.r2.dev/pita-o-funk-super-slowed.mp3",
+          url: "https://audio.phonktopia.com/pita-o-funk-super-slowed.mp3",
         },
         {
           metaData: {
             artist: "Phonk",
             title: "Qaraqshy Z Omega",
           },
-          url: "https://pub-47d44b4e2e4c43d7922bf3cc7715f8b0.r2.dev/qaraqshy-z-omega-slowed.mp3",
+          url: "https://audio.phonktopia.com/qaraqshy-z-omega-slowed.mp3",
         },
         {
           metaData: {
             artist: "Phonk",
             title: "X Menor",
           },
-          url: "https://pub-47d44b4e2e4c43d7922bf3cc7715f8b0.r2.dev/x-menor-speed-up-soulfusi-wxrtnax.mp3",
+          url: "https://audio.phonktopia.com/x-menor-speed-up-soulfusi-wxrtnax.mp3",
         },
         {
           metaData: {
@@ -161,21 +161,21 @@ document.addEventListener('DOMContentLoaded', function () {
             artist: "Phonk",
             title: "X Mortal",
           },
-          url: "https://pub-47d44b4e2e4c43d7922bf3cc7715f8b0.r2.dev/x-mortal-super-slowed.mp3",
+          url: "https://audio.phonktopia.com/x-mortal-super-slowed.mp3",
         },
         {
           metaData: {
             artist: "Phonk",
             title: "Z Alpha2",
           },
-          url: "https://pub-47d44b4e2e4c43d7922bf3cc7715f8b0.r2.dev/z-alpha2.mp3",
+          url: "https://audio.phonktopia.com/z-alpha2.mp3",
         },
         {
           metaData: {
             artist: "Phonk",
             title: "Z Ispugalsa",
           },
-          url: "https://pub-47d44b4e2e4c43d7922bf3cc7715f8b0.r2.dev/z-ispugalsa-ultra-slowed.mp3",
+          url: "https://audio.phonktopia.com/z-ispugalsa-ultra-slowed.mp3",
         }
       ],
       zIndex: 9999,

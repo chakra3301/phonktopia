3d3ad4b68c6e4244ccb672a1673d9f320d448c88
Luca Orion <lucaorion@Lucas-MacBook-Pro.local>
2025-06-15 22:55:23 -0400

    Update CSS, JavaScript, and HTML files

--- src/scripts/video-feed.js
+++ src/scripts/video-feed.js
@@ -66,7 +66,7 @@ function renderCarouselVideo(index) {
   video.autoplay = true;
   video.controlsList = 'nodownload nofullscreen noremoteplayback';
   video.setAttribute('disablePictureInPicture', '');
-  video.controls = true;
+  video.controls = false;
 
   const frame = document.createElement('img');
   frame.src = 'assets/videoframe.png';
